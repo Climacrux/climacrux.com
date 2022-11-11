66b5dc72f2b8d460b4d6ad10db7aac7abbd50ea4
Fabienne Muff <fabienne.muff@sunrise.net>
2022-11-11 08:20:44 +0100

    adding footer component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Navigation from '../components/Navigation'
+import Footer from '../components/Footer'
 
 const Home: NextPage = () => {
   return (<>
@@ -69,19 +70,19 @@ const Home: NextPage = () => {
           </a>
         </div>
       </main>
-
       <footer className="flex h-24 w-full items-center justify-center border-t">
         <a
           className="flex items-center justify-center gap-2"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           Powered by{' '}
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </a>
       </footer>
     </div>
+          <Footer></Footer>
   </>
   )
 }
