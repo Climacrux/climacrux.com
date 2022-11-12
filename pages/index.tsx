3dfc3d5f60dcfc86a885363f9e8527732ffc14a0
Fabienne Muff <fabienne.muff@sunrise.net>
2022-11-12 21:29:58 +0100

    adding fav icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Navigation from '../components/Navigation'
 import Footer from '../components/Footer'
 import CardList from '../components/CardList'
 import HeroSection from '../components/HeroSection'
 
 const Home: NextPage = () => {
   return (<>
-      <HeroSection></HeroSection>
+    <Head>
+      <title>Climacrux</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <HeroSection></HeroSection>
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
