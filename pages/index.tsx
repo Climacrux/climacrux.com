267ae28498704db72088243d2df6e08ca44a0086
Fabienne Muff <fabienne.muff@sunrise.net>
2022-11-12 15:29:17 +0100

    removing inbuilt footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Navigation from '../components/Navigation'
 import Footer from '../components/Footer'
 import CardList from '../components/CardList'
@@ -74,19 +73,8 @@ const Home: NextPage = () => {
         </div>
         <CardList></CardList>
       </main>
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
-          <Footer></Footer>
+    <Footer></Footer>
   </>
   )
 }
